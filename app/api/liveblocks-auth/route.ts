4f00ce320ab923b58f14ca3ef734765720848844
nurhussen muhammed <nurhussenm80@gmail.com>
2024-12-28 11:31:26 -0800

    fixing typos

--- app/api/liveblocks-auth/route.ts
+++ app/api/liveblocks-auth/route.ts
@@ -1,50 +1,36 @@
-// import { Liveblocks } from "@liveblocks/node";
 import { liveblocks } from "@/lib/liveblocks";
 import { getUserColor } from "@/lib/utils";
 import { currentUser } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 
-
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export async function POST(request: Request) {
-    const clerkUser = await currentUser()
+  const clerkUser = await currentUser();
 
-    if(!clerkUser) redirect('/sign-in')
+  if(!clerkUser) redirect('/sign-in');
+
+  const { id, firstName, lastName, emailAddresses, imageUrl } = clerkUser;
+
   // Get the current user from your database
-
-    const {id, firstName, lastName, emailAddresses, imageUrl} = clerkUser;
   const user = {
     id,
     info: {
-        id,
-        name:  `${firstName} ${lastName}`,
-        email:  emailAddresses[0].emailAddress,
-        avatar: imageUrl,
-        color: getUserColor(id)
-
+      id,
+      name: `${firstName} ${lastName}`,
+      email: emailAddresses[0].emailAddress,
+      avatar: imageUrl,
+      color: getUserColor(id),
     }
   }
 
-
-
-  // Start an auth session inside your endpoint
-  const session = liveblocks.prepareSession(
-    user.id,
-    { userInfo: user.info } // Optional
-  );
-
-  // Use a naming pattern to allow access to rooms with wildcards
-  // Giving the user read access on their org, and write access on their group
-  session.allow(`${user.organization}:*`, session.READ_ACCESS);
-  session.allow(`${user.organization}:${user.group}:*`, session.FULL_ACCESS);
-
-  // Authorize the user and return the result
+  // Identify the user and return the result
   const { status, body } = await liveblocks.identifyUser(
     {
-        userId: user.info.email,
-        groupIds: []
+      userId: user.info.email,
+      groupIds: [],
     },
-    { userInfo: user.info }
-  )
-//   session.authorize();
+    { userInfo: user.info },
+  );
+
   return new Response(body, { status });
 }
